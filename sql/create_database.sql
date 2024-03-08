0ce88720acb58a008b237fcfbe8f2dcc75da70d7
doh43 <ohdaniel43@gmail.com>
2024-03-08 13:40:13 -0500

    Create database script

--- sql/create_database.sql
+++ sql/create_database.sql
@@ -1,3 +1,22 @@
 CREATE DATABASE rfid_database;
 USE rfid_database;
-CREATE TABLE scans (id INT AUTO_INCREMENT PRIMARY KEY, tag_id VARCHAR(255), scan_time TIMESTAMP);
+
+CREATE TABLE users (
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) NOT NULL, 
+    email VARCHAR(255),
+);
+
+CREATE TABLE admins (
+    admin_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT,
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+
+CREATE TABLE scans (
+    scan_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT,  -- to record which user made the scan
+    tag_id VARCHAR(255), -- RFID tag identifier
+    scan_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
